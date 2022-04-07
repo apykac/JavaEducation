53f8993727a3526043be1b3ab3c16e941ff90134
Ермаков Денис <denis.ermakov@gazprombank.ru>
2022-04-07 21:08:09 +0300

    Fix description of homework9, add homework 15-17

---
+++ src/main/java/ru/gazprombank/educate/homework17/HomeWork.java
@@ -0,0 +1,8 @@
+package ru.gazprombank.educate.homework17;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/ru/gazprombank/educate/homework17/HomeworkTest.java
@@ -0,0 +1,70 @@
+package ru.gazprombank.educate.homework17;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.gazprombank.educate.test.util.AssertUtils;
+import ru.gazprombank.educate.test.util.Modifier;
+import ru.gazprombank.educate.test.util.reflection.TestClass;
+import ru.gazprombank.educate.test.util.reflection.TestMethod;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HomeworkTest {
+
+    private TestMethod calculateWordsCountMethod;
+
+    @BeforeEach
+    void setUp() {
+        TestClass testClass = AssertUtils.getClass("ru.gazprombank.educate.homework17.HomeWork");
+        calculateWordsCountMethod = AssertUtils.getMethod(testClass,
+                "wordsCount",
+                int.class.getName(),
+                Modifier.modifiersToList(Modifier.PUBLIC, Modifier.STATIC),
+                List.of(String[].class.getName(), int.class.getName()));
+    }
+
+    @DisplayName("words: ['a', 'bb', 'b', 'ccc'], len: 1; answer = 2")
+    @Test
+    void test1() {
+        int fine = calculateWordsCountMethod.invokeStaticMethod(new String[]{"a", "bb", "b", "ccc"}, 1);
+        assertEquals(2, fine, "Expected string: 2 actual " + fine);
+    }
+
+    @DisplayName("words: ['a', 'bb', 'b', 'ccc'], len: 3; answer = 1")
+    @Test
+    void test2() {
+        int fine = calculateWordsCountMethod.invokeStaticMethod(new String[]{"a", "bb", "b", "ccc"}, 3);
+        assertEquals(1, fine, "Expected string: 1 actual " + fine);
+    }
+
+    @DisplayName("words: ['a', 'bb', 'b', 'ccc'], len: 4; answer = 0")
+    @Test
+    void test3() {
+        int fine = calculateWordsCountMethod.invokeStaticMethod(new String[]{"a", "bb", "b", "ccc"}, 4);
+        assertEquals(0, fine, "Expected string: 0 actual " + fine);
+    }
+
+    @DisplayName("words: ['xx', 'yyy', 'x', 'yy', 'z'], len: 1; answer = 2")
+    @Test
+    void test4() {
+        int fine = calculateWordsCountMethod.invokeStaticMethod(new String[]{"xx", "yyy", "x", "yy", "z"}, 1);
+        assertEquals(2, fine, "Expected string: 2 actual " + fine);
+    }
+
+    @DisplayName("words: ['xx', 'yyy', 'x', 'yy', 'z'], len: 2; answer = 2")
+    @Test
+    void test5() {
+        int fine = calculateWordsCountMethod.invokeStaticMethod(new String[]{"xx", "yyy", "x", "yy", "z"}, 2);
+        assertEquals(2, fine, "Expected string: 2 actual " + fine);
+    }
+
+    @DisplayName("words: ['xx', 'yyy', 'x', 'yy', 'z'], len: 3; answer = 1")
+    @Test
+    void test6() {
+        int fine = calculateWordsCountMethod.invokeStaticMethod(new String[]{"xx", "yyy", "x", "yy", "z"}, 3);
+        assertEquals(1, fine, "Expected string: 1 actual " + fine);
+    }
+}
