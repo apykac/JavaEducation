71a6f356c4a53af510974803e0b1ceb92b7099a5
Viktor Turanskiy <cao2.c19@gmail.com>
2022-04-07 18:10:03 +0300

    Homework1 & Homework2 Done

--- src/main/java/ru/gazprombank/educate/homework1/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework1/Homework.java
@@ -4,4 +4,20 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+    public static int punctuationCount(String a){
+        char[]c={'!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-',
+                '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^',
+                '_', '`', '{', '|', '}', '~'};
+        int count = 0;
+        char[]ch = a.toCharArray();
+        for(int i=0;i<ch.length;i++) {
+            for (int j = 0; j < c.length; j++) {
+                if (ch[i] == c[j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
 }
