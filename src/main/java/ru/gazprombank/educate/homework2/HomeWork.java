2a994ed9d25018d759ab57ea01348067b8b0e2cd
dimi1309 <dim1309@gmail.com>
2022-04-08 14:20:50 +0300

    Continue homework2

--- src/main/java/ru/gazprombank/educate/homework2/HomeWork.java
+++ src/main/java/ru/gazprombank/educate/homework2/HomeWork.java
@@ -6,45 +6,28 @@ package ru.gazprombank.educate.homework2;
  */
 public class HomeWork {
 
-    public double a;
-    public double b;
-    public double c;
-
-    public HomeWork(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    public static double semiPer(double a, double b, double c) {
+    public static double semiPer(int a, int b, int c) {
         return (a + b + c) / 2;
     }
 
-    public static double calculateTriangleArea(double a, double b, double c) {
+    public static double calculateTriangleArea(int a, int b, int c) {
         return Math.sqrt(semiPer(a, b, c) * (semiPer(a, b, c) - a) * (semiPer(a, b, c) - b) * (semiPer(a, b, c) - c));
     }
 
-    public static void main(String[] calculateTriangleArea) throws TriangleExistException {
+    public static void main(String[] args) throws TriangleExistException {
         double resultCalculation;
 
-        HomeWork triangleArea = new HomeWork(8, 4, 7);
+        TriangleArea triangleArea = new TriangleArea(8, 4, 7);
 
-        try {
-            if ((triangleArea.a + triangleArea.b <= triangleArea.c) ||
-                    (triangleArea.a + triangleArea.c <= triangleArea.b) ||
-                    (triangleArea.b + triangleArea.c <= triangleArea.a)) {
+        if ((triangleArea.a + triangleArea.b <= triangleArea.c) ||
+                (triangleArea.a + triangleArea.c <= triangleArea.b) ||
+                (triangleArea.b + triangleArea.c <= triangleArea.a)) {
 
-                throw new TriangleExistException("Треугольников с такими сторонами не бывает. " +
-                        "Одна сторона больше суммы двух других сторон.");
-            }
-
-            resultCalculation = HomeWork.calculateTriangleArea(triangleArea.a,
-                    triangleArea.b, triangleArea.c);
-            System.out.printf("Площадь треугольника со сторонами " + triangleArea.a + ", " +
-                    triangleArea.b + ", " + triangleArea.c + " равна " + "%.3f", resultCalculation);
-        } catch (TriangleExistException e) {
-            System.out.println(e.getMessage());
+            throw new TriangleExistException("triangle not exist");
         }
 
+        resultCalculation = calculateTriangleArea(triangleArea.a, triangleArea.b, triangleArea.c);
+        System.out.printf("Площадь треугольника со сторонами " + triangleArea.a + ", " +
+                triangleArea.b + ", " + triangleArea.c + " равна " + "%.3f", resultCalculation);
     }
 }
---
+++ src/main/java/ru/gazprombank/educate/homework2/TriangleArea.java
@@ -0,0 +1,13 @@
+package ru.gazprombank.educate.homework2;
+
+public class TriangleArea {
+    public int a;
+    public int b;
+    public int c;
+
+    public TriangleArea(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
