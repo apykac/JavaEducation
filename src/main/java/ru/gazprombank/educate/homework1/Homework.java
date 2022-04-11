e3c3fb1fc8112e7094eb362863d9a9a721d03a2b
dimi1309 <dim1309@gmail.com>
2022-04-11 09:26:27 +0300

    Done homework1, done homework2

--- src/main/java/ru/gazprombank/educate/homework1/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework1/Homework.java
@@ -1,6 +1,29 @@
 package ru.gazprombank.educate.homework1;
 
 public class Homework {
+
+    public static int punctuationCount(String anyString) {
+
+        int count = 0;
+        for (int i = 0; i < anyString.length() - 1; i++) {
+            if (anyString.charAt(i) == ',' || anyString.charAt(i) == ';' || anyString.charAt(i) == '.' ||
+                    anyString.charAt(i) == '!' || anyString.charAt(i) == ':' || anyString.charAt(i) == '?' ||
+                    anyString.charAt(i) == '(' || anyString.charAt(i) == ')' || anyString.charAt(i) == '-' ||
+                    anyString.charAt(i) == '<' || anyString.charAt(i) == '>' || anyString.charAt(i) == '#' ||
+                    anyString.charAt(i) == '$' || anyString.charAt(i) == '%' || anyString.charAt(i) == '&' ||
+                    anyString.charAt(i) == '*' || anyString.charAt(i) == '+' || anyString.charAt(i) == '/' ||
+                    anyString.charAt(i) == '=' || anyString.charAt(i) == '@' || anyString.charAt(i) == '[' ||
+                    anyString.charAt(i) == ']' || anyString.charAt(i) == '\\' || anyString.charAt(i) == '^' ||
+                    anyString.charAt(i) == '_' || anyString.charAt(i) == '|' || anyString.charAt(i) == '~' ||
+                    anyString.charAt(i) == '`' || anyString.charAt(i) == '\"' || anyString.charAt(i) == '\'' ||
+                    anyString.charAt(i) == '{' || anyString.charAt(i) == '}'
+            )
+                count++;
+        }
+
+        return (count);
+    }
+
     public static void main(String[] args) {
 
     }
