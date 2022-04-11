8be2177c63e741be7fe6c2c0ce113adaa3eb830a
SergeyPannikov <panplex@mail.ru>
2022-04-11 12:10:50 +0300

    dz18

--- src/main/java/ru/gazprombank/educate/homework18/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework18/Homework.java
@@ -3,6 +3,18 @@ package ru.gazprombank.educate.homework18;
 public class Homework {
 
     public static void main(String[] args) {
+        System.out.println(matchingChar(new String[]{"aa", "bb", "cc"}, new String[]{"aaa", "xx", "bb"}));
 
+    }
+    public static int matchingChar(String[] a, String[] b) {
+        int result = 0;
+        for(int i = 0 ; i < a.length;i++){
+            if (a[i] != "" && b[i] !="") {
+                if (a[i].charAt(0) == b[i].charAt(0)) {
+                    result++;
+                }
+            }
+        }
+        return result;
     }
 }
