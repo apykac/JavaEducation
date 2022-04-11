552328897fa3dab1f1c1a477dc3c2d40c6f6a8fa
SergeyPannikov <panplex@mail.ru>
2022-04-11 13:34:32 +0300

    dz19

--- src/main/java/ru/gazprombank/educate/homework19/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework19/Homework.java
@@ -3,6 +3,31 @@ package ru.gazprombank.educate.homework19;
 public class Homework {
 
     public static void main(String[] args) {
+        System.out.println(longestStreak("xxXXXX"));
 
+    }
+    public static int longestStreak(String str){
+        int count = 1;
+        int[] result = new int[str.length()];
+        int maxCount = 0 ;
+        if (str != "") {
+            for (int i = 0; i < str.length() - 1; i++) {
+                for (int j = i + 1; j < str.length(); j++) {
+                    if (str.charAt(i) == str.charAt(j)) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+                result[i] = count;
+                count = 1;
+            }
+            for (int i = 0; i < result.length; i++) {
+                if (result[i] > maxCount) {
+                    maxCount = result[i];
+                }
+            }
+        }
+        return maxCount;
     }
 }
