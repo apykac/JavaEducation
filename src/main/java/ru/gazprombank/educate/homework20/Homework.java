a46af20257a3ae26218b5142282b5ac54dbcabc2
SergeyPannikov <panplex@mail.ru>
2022-04-11 16:03:30 +0300

    dz20

--- src/main/java/ru/gazprombank/educate/homework20/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework20/Homework.java
@@ -3,6 +3,16 @@ package ru.gazprombank.educate.homework20;
 public class Homework {
 
     public static void main(String[] args) {
-
+        System.out.println(yoYo("xhyoxxyo"));
     }
+    public static int yoYo(String str){
+        int count = 0;
+        if (str.indexOf("yo") == -1) {
+            return count;
+        }
+        str = str.substring(str.indexOf("yo") + 2, str.length());
+        count = count + 1;
+        return count + yoYo(str);
+    }
+
 }
