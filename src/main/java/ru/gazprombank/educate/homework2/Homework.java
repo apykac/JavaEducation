51092edbff390f04b74c614f716db20f1af70ef4
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-04-12 17:07:19 +0300

    Homework2

--- src/main/java/ru/gazprombank/educate/homework2/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework2/Homework.java
@@ -1,7 +1,17 @@
 package ru.gazprombank.educate.homework2;
 
-public class Homework {
-    public static void main(String[] args) {
+import java.awt.geom.Area;
 
+public class Homework {
+    public static void main(String[] args) throws TriangleExistException {
+        System.out.println(Homework.calculateTriangleArea(10, 8, 18));
     }
+        public static double calculateTriangleArea(int a, int b, int c) throws TriangleExistException {
+        if (a+b<=c) {
+            throw new TriangleExistException();
+        }
+        int p = (a+b+c)/2;
+        return  Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+            }
 }
