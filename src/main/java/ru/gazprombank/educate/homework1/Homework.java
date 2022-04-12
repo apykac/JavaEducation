45792e31e512c022964a3d8dc10ccc4a09b8923b
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-04-12 14:16:35 +0300

    Homework1

--- src/main/java/ru/gazprombank/educate/homework1/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework1/Homework.java
@@ -2,6 +2,25 @@ package ru.gazprombank.educate.homework1;
 
 public class Homework {
     public static void main(String[] args) {
+        String str1 = "What, where,when?";
+        System.out.println(punctuationCount(str1));
+    }
 
+
+    public static int punctuationCount(String str1) {
+        char[] punctuationArray = {',', '?'};
+        int count = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            for (char c : punctuationArray) {
+                if (str1.charAt(i) == c) {
+                    count++;
+                    break;
+                }
+            }
+
+        }
+        return count;
     }
 }
+
+
