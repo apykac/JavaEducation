e41a8e9f8b682721fbbb2e10095d2e73b54f3bc3
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-04-13 09:38:08 +0300

    Homework3

--- src/main/java/ru/gazprombank/educate/homework3/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework3/Homework.java
@@ -1,7 +1,21 @@
 package ru.gazprombank.educate.homework3;
 
+import java.util.Arrays;
+
 public class Homework {
     public static void main(String[] args) {
+        int[][] array = new int[2][2];
 
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[1][0] = 3;
+        array[1][1] = 4;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
