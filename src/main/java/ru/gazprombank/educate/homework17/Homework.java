17781e6e7d36dabd2a63f5f51574aa81d7d5b631
dimi1309 <dim1309@gmail.com>
2022-04-17 19:13:17 +0300

    Done homework 15, 17

--- src/main/java/ru/gazprombank/educate/homework17/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework17/Homework.java
@@ -1,6 +1,17 @@
 package ru.gazprombank.educate.homework17;
 
 public class Homework {
+    public static int wordsCount(String[] words, int len) {
+        int num = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() == len) {
+                num = num + 1;
+            }
+        }
+
+        return num;
+
+    }
 
     public static void main(String[] args) {
 
