4143719ee6afc6213142b7f349717d2289238ee3
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-04-28 16:59:39 +0300

    Homework7

--- src/main/java/ru/gazprombank/educate/homework7/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework7/Homework.java
@@ -3,6 +3,13 @@ package ru.gazprombank.educate.homework7;
 public class Homework {
 
     public static void main(String[] args) {
-
+        twoSumOne(2, 4, 7);
+        System.out.println(twoSumOne(2, 4, 7));
     }
-}
+
+    public static boolean twoSumOne(int a, int b, int c) {
+        return  a + b == c || a + c == b || b + c == a;
+
+        }
+    }
+
