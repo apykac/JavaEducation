bf7531d1731eee3110862db52ce19006d16ccaa0
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-05-05 15:16:06 +0300

    Homework12

--- src/main/java/ru/gazprombank/educate/homework12/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework12/Homework.java
@@ -3,6 +3,22 @@ package ru.gazprombank.educate.homework12;
 public class Homework {
 
     public static void main(String[] args) {
+        sumUnique(4, 4, 3);
+    }
 
+    public static void sumUnique(int a, int b, int c) {
+        int sum = a + b + c;
+        if (a != b && b != c && a != c) {
+            System.out.println(sum);
+        }
+        if (a == b) {
+            System.out.println(c);
+        }
+        if (a == c) {
+            System.out.println(b);
+        }
+        if (b == c) {
+            System.out.println(a);
+        }
     }
 }
