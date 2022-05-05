8c93d14e149b36e8e1cf68a9c43655f8e7ffcc01
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-05-05 14:33:58 +0300

    Homework11

--- src/main/java/ru/gazprombank/educate/homework11/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework11/Homework.java
@@ -3,6 +3,20 @@ package ru.gazprombank.educate.homework11;
 public class Homework {
 
     public static void main(String[] args) {
-
+        fizzyWizzy(2);
     }
+
+    public static void fizzyWizzy(int n) {
+        if (n % 3 == 0 && n % 5 !=0 ) {
+            System.out.println("Fizz!");
+        }
+        if (n % 5 == 0 && n % 3 !=0 ) {
+            System.out.println("Buzz!");
+        }
+        if (n % 3 == 0 && n % 5 == 0) {
+            System.out.println("FizzBuzz!");
+        } else  {
+            System.out.println(n + "!");
+        }
+            }
 }
