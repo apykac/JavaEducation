d02b9461e85d9ff243a7066d37707d07f814fb80
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-05-05 18:49:07 +0300

    Homework13

--- src/main/java/ru/gazprombank/educate/homework13/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework13/Homework.java
@@ -3,6 +3,20 @@ package ru.gazprombank.educate.homework13;
 public class Homework {
 
     public static void main(String[] args) {
-
+        System.out.println(nearestTwentyOne(20, 19));
     }
+
+    public static int nearestTwentyOne(int a, int b) {
+        if (21 - a >= 0 && 21 - b >= 0){
+            return Math.max(a,b);
+        }
+        if (21 - a < 0){
+            return b;
+                    }
+        if (21 - b < 0){
+            return a;
+        }
+        return 0;
+            }
+
 }
