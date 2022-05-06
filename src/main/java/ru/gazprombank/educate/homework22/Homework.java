37dc4bba329ea44504efce125ad32643e947ea79
RSharapova <rz.sharapova@gmail.com>
2022-05-06 10:14:47 +0500

    Выполнены 22-23 ДЗ

--- src/main/java/ru/gazprombank/educate/homework22/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework22/Homework.java
@@ -3,22 +3,18 @@ package ru.gazprombank.educate.homework22;
 public class Homework {
 
     public static void main(String[] args) {
-//        System.out.println(hyphenSplit("hooho"));
+        System.out.println(hyphenSplit("hoohh "));
     }
 
-//    public static String hyphenSplit(String str) {
-//        if (str.length() < 1)
-//            return "";
-//        String newStr = str.substring(1);
-//        String letter = String.valueOf(str.charAt(0));
-//
-//        if (str.charAt(0) == str.charAt(1)) {
-//            return str.replaceFirst("letter", letter + "-") + hyphenSplit(newStr);
-//
-//        } else {
-//            return str.charAt(0) + hyphenSplit(newStr);
-//        }
-//    }
+    public static String hyphenSplit(String str) {
+        String newStr = str.substring(1);
+        if (newStr.length() < 1)
+            return "";
 
-
+        if (str.charAt(0) == str.charAt(1)) {
+            return str.substring(0, 1) + "-" + hyphenSplit(newStr.substring(0));
+        } else {
+            return str.substring(0, 1) + hyphenSplit(newStr.substring(0));
+        }
+    }
 }
--- src/main/java/ru/gazprombank/educate/homework23/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework23/Homework.java
@@ -3,6 +3,13 @@ package ru.gazprombank.educate.homework23;
 public class Homework {
 
     public static void main(String[] args) {
+        System.out.println(insideBrackets("HEL[abc]LO"));
 
     }
+
+    public static String insideBrackets(String str) {
+        return str.substring(str.indexOf("["), str.indexOf("]") + 1);
+
+    }
+
 }
