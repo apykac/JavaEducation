956a5b666c6077982c3cdc53669ccac4ee4966df
RSharapova <rz.sharapova@gmail.com>
2022-05-11 11:42:55 +0500

    Выполнено 24-25 ДЗ

--- src/main/java/ru/gazprombank/educate/homework24/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework24/Homework.java
@@ -1,8 +1,28 @@
 package ru.gazprombank.educate.homework24;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Homework {
 
     public static void main(String[] args) {
+        System.out.println(getMapFromString("123451222"));
+    }
 
+    public static Map<Integer, Integer> getMapFromString(String str) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i <10; i++) {
+            int a = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (String.valueOf(str.charAt(j)).equals(String.valueOf(i))) {
+                    a++;
+                }
+            }
+            if (a > 0) {
+                map.put(i, a);
+            }
+        }
+        return map;
     }
 }
--- src/main/java/ru/gazprombank/educate/homework25/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework25/Homework.java
@@ -1,8 +1,30 @@
 package ru.gazprombank.educate.homework25;
 
+import java.util.Arrays;
+
 public class Homework {
 
     public static void main(String[] args) {
+        String[] str = {"test2", "test2", "test2Tr", "te"};
+        System.out.println(getPrefix(str));
+    }
+
+    public static String getPrefix(String[] strings) {
+        int size = strings.length;
+
+        if (size == 0)
+            return "";
+
+        if (size == 1)
+            return strings[0];
+
+        Arrays.sort(strings);
+        int min = Math.min(strings[0].length(), strings[size - 1].length());
+        int i = 0;
+        while (i < min && strings[0].charAt(i) == strings[size - 1].charAt(i))
+            i++;
+        String prefix = strings[0].substring(0, i);
+        return prefix;
 
     }
 }
