06b04a004d48c07d6f5867d92d913895414eb53f
ElenaButa <elenabutakova567@gmail.com>
2022-05-11 10:04:58 +0500

    Added homework11

--- src/main/java/ru/gazprombank/educate/homework11/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework11/Homework.java
@@ -1,8 +1,34 @@
 package ru.gazprombank.educate.homework11;
 
 public class Homework {
+    /*
+    FizzBuzz головоломка
+    - сделать статический публичный метод в `HomeWork` под названием `fizzyWizzy`
+    - метод должен принимать на вход 1 параметр `int n`
+    - метод должен вернуть строковое представление числа со знаком `!` в конце
+    - если число делится на 3, то надо вернуть `Fizz!`
+    - если число делится на 5, то надо вернуть `Buzz!`
+    - если число делится и на 3 и на 5, то надо вернуть `FizzBuzz!`
+    Пример
+    ```
+    fizzyWizzy(1) - вернет "1!"
+    fizzyWizzy(2) - вернет "2!"
+    fizzyWizzy(3) - вернет "Fizz!"
+    ```
+     */
 
     public static void main(String[] args) {
-
+        int n = 1;
+        System.out.println(fizzyWizzy(n));
     }
+
+    public static String fizzyWizzy(int n) {
+        String s = null;
+        if (n % 3 == 0) s = "Fizz!";
+        if (n % 5 == 0) s = "Buzz!";
+        if (n % 3 == 0 && n % 5 == 0) s = "FizzBuzz!";
+        if (s == null) s = String.valueOf(n) + "!";
+        return s;
+    }
+
 }
