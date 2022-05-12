1d5d59a01b731fb5651deeb92e9964edab4e6525
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-05-12 15:32:25 +0300

    Homework15

--- src/main/java/ru/gazprombank/educate/homework15/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework15/Homework.java
@@ -1,8 +1,20 @@
 package ru.gazprombank.educate.homework15;
 
+import java.util.Arrays;
+
 public class Homework {
 
     public static void main(String[] args) {
-
+        System.out.println(repeatChar("Month"));
     }
+
+    public static String repeatChar(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            result = result + str.charAt(i) + str.charAt(i);
+
+        }
+        return result;
+    }
+
 }
