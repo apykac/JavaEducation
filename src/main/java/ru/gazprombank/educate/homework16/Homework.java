da2bd9e6bdfdb81e59602ea8634e512c4ecec5f4
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-05-16 20:08:45 +0300

    Homework16

--- src/main/java/ru/gazprombank/educate/homework16/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework16/Homework.java
@@ -3,6 +3,18 @@ package ru.gazprombank.educate.homework16;
 public class Homework {
 
     public static void main(String[] args) {
+        System.out.println(starKill("ab**cd"));
+    }
+
+    public static String starKill(String str) {
+        while (str.contains("*")) {
+            str = str.replaceAll(".{1}\\*{1,}.{1}", "");
+        }
+        return str;
+
 
     }
+
 }
+
+
