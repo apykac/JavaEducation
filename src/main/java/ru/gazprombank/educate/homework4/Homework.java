c244c0ea7a5cbd902872df246402837cb6335d75
Core <zergi89@mail.ru>
2022-05-19 15:47:28 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework4/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework4/Homework.java
@@ -2,6 +2,10 @@ package ru.gazprombank.educate.homework4;
 
 public class Homework {
     public static void main(String[] args) {
-
+        int[] array = new int[33];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int)(Math.random() * 33) - 11);
+            System.out.println(array[i]);
+        }
     }
 }
