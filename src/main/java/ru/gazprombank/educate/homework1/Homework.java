863a70cdc12ab4b9f0be0c51a8dc85acfc224e70
Core <zergi89@mail.ru>
2022-05-19 12:54:40 +0300

    home v1

--- src/main/java/ru/gazprombank/educate/homework1/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework1/Homework.java
@@ -4,19 +4,12 @@ package ru.gazprombank.educate.homework1;
 import java.util.Arrays;
 
 public class Homework {
-    public static void main(final String[] args) {
-        String text = "В=опрос. о смысле, жизни также может, пониматься как субъективная!!! оценка!!стая целью жизни?";
-        int counter = 0;
-        String [] punctuations = {"",".", "!", ";", "?"};
-        for (int i = 0; i < text.length(); i++){
-            if (Arrays.asList(punctuations).contains(String.valueOf(text.charAt(i)))){
-                // Arrays.asList(punctuations) - метод
-                // String.valueOf(text.charAt(i)) - возвращает строковое представление переданного аргумента.
-                // contains -это метод Java, позволяющий проверить, содержит ли String другую подстроку или нет. Возвращает логическое значение, поэтому его можно использовать непосредственно внутри операторов if.
-                counter++;
-            }
+    public static void main(String[] args) {
+        String punctuation = "В=опрос. о смысле, жизни также может, пониматься как субъективная!!! оценка!!стая целью жизни?";
+        int count = 0;
+        for(int i = 0; i < punctuation.length(); i++) {
+            count++;
         }
-
-        System.out.println("количество знаков припинания: " + counter);
+        System.out.println("Колличество в тексте знаков  пунктуации " + count);
     }
 }
