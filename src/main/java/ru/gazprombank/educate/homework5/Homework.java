07c9a1d79308ca126db335761133ccab493738e3
Core <zergi89@mail.ru>
2022-05-19 16:01:45 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework5/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework5/Homework.java
@@ -3,6 +3,16 @@ package ru.gazprombank.educate.homework5;
 public class Homework {
 
     public static void main(String[] args) {
+        String f = "InsideTheFire";
+        System.out.println(middleThree(f));
+    }
 
+    public static String middleThree(String f){
+        int x = (f.length() / 2);
+        if (f.length() <= 3) {
+            return f;
+
+        }
+        return f.substring(x - 1, x + 2);
     }
 }
