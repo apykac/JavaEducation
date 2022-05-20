3a34be1ff1e43352b1e7c910e6e11912864a7009
Core <zergi89@mail.ru>
2022-05-20 17:44:36 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework8/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework8/Homework.java
@@ -1,8 +1,13 @@
 package ru.gazprombank.educate.homework8;
 
-public class Homework {
+     public class Homework {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+
+        }
+
+        public static boolean birdsSinging(int temp, boolean isSummer){
+            return ((isSummer) && (temp>=60 && temp<=100)) || (temp>=60 && temp<=90);
+        }
 
     }
-}
