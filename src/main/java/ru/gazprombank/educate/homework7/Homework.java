b55342605a24a05e5da7dd9a6973b069820cc516
Core <zergi89@mail.ru>
2022-05-20 17:42:13 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework7/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework7/Homework.java
@@ -7,7 +7,6 @@ public class Homework {
     }
 
     public static boolean twoSumOne(int a, int b, int c){
-       if  (a + b == c || a + c == b || b + c == a) return true;
-       return false;
+        return a + b == c || a + c == b || b + c == a;
     }
 }
