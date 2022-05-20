d4fc1b392c59c9ebcaf52f956a86d545d5d3c166
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-05-20 17:15:57 +0300

    Homework18

--- src/main/java/ru/gazprombank/educate/homework18/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework18/Homework.java
@@ -3,6 +3,18 @@ package ru.gazprombank.educate.homework18;
 public class Homework {
 
     public static void main(String[] args) {
+        String[] a = new String[]{"aa", "", " cc"};
+        String[] b = new String[]{"aa", "dd", "jj"};
+        System.out.println(matchingChar(a, b));
+    }
 
+    private static int matchingChar(String[] a, String[] b) {
+        int counter = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (!a [i].equals("") && b[i].startsWith(String.valueOf(a[i].charAt(0)))) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
