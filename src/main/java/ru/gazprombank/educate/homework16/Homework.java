99c7a971e76da4943cef1809d1ec6ba560c1e36e
dimi1309 <dim1309@gmail.com>
2022-05-21 19:49:58 +0300

    Done homework 16, 19

--- src/main/java/ru/gazprombank/educate/homework16/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework16/Homework.java
@@ -1,6 +1,38 @@
 package ru.gazprombank.educate.homework16;
 
 public class Homework {
+    public static String starKill(String anyString) {
+
+        String newStr = null;
+        int indexStar = anyString.indexOf("*");
+        if (anyString.equals("*")) {
+            anyString = anyString.replace("*", "");
+            return anyString;
+        }
+        if (indexStar == -1) {
+            return anyString;
+        } else if (anyString.startsWith("*")) {
+            newStr = anyString.substring(indexStar + 2);
+            newStr = starKill(newStr);
+
+        } else if (anyString.endsWith("*")) {
+            newStr = anyString.substring(0, indexStar - 1);
+            newStr = starKill(newStr);
+        } else if (anyString.contains("**")) {
+            newStr = anyString.substring(0, anyString.indexOf("*")) + anyString.substring(anyString.indexOf("*") + 1);
+            newStr = starKill(newStr);
+        } else if (anyString.contains("***")) {
+            newStr = anyString.substring(0, anyString.indexOf("*")) + anyString.substring(anyString.indexOf("*") + 2);
+            newStr = starKill(newStr);
+        } else {
+
+            for (int i = 0; i < anyString.length() - 1; i++) {
+                newStr = anyString.substring(0, anyString.indexOf("*") - 1) + anyString.substring(anyString.indexOf("*") + 2);
+                newStr = starKill(newStr);
+            }
+        }
+        return newStr;
+    }
 
     public static void main(String[] args) {
 
