0c99b284a930b78b5ab2553ec1673202a4eab919
Core <zergi89@mail.ru>
2022-05-23 16:46:01 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework10/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework10/Homework.java
@@ -3,6 +3,16 @@ package ru.gazprombank.educate.homework10;
 public class Homework {
 
     public static void main(String[] args) {
+        int n = 24;
+        System.out.println(isCool(n));
 
+    }
+
+    public static boolean isCool(int n) {
+        if (n % 11 == 0 || n % 11 == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
