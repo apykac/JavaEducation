5ad67f6c1a9e4547e3479b3e146dbc696561c165
Core <zergi89@mail.ru>
2022-05-23 16:39:24 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework9/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework9/Homework.java
@@ -2,7 +2,24 @@ package ru.gazprombank.educate.homework9;
 
 public class Homework {
 
-    public static void main(String[] args) {
+    public class Homework {
+        public static boolean isOrdered(int first, int second, int third, boolean itsOk) {
+            if (itsOk == true) {
+                if ((second <= first || second > first) && (second < third)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if ((second > first) && (third > second)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
 
+        public static void main(String[] args) {
+
+        }
     }
-}
