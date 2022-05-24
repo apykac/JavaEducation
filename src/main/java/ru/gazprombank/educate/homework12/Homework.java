4b7425cdd094900d5c163dffbcad056ec0b20c48
Core <zergi89@mail.ru>
2022-05-24 16:52:25 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework12/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework12/Homework.java
@@ -1,8 +1,26 @@
 package ru.gazprombank.educate.homework12;
 
 public class Homework {
+        public static int sumUnique(int a, int b, int c) {
+            int sum;
+            if ((a == b) && (a == c) && (b == c)) {
+                return 0;
+            } else if (a == b) {
+                sum = c;
+                return c;
+            } else if (a == c) {
+                sum = b;
+                return b;
+            } else if (b == c) {
+                sum = a;
+                return a;
+            } else {
+                sum = a + b + c;
+                return sum;
+            }
+        }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
+        }
     }
-}
