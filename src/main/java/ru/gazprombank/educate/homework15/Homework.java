e8aee94fe54a5e8f77ac8927b4c766f933ab505b
Core <zergi89@mail.ru>
2022-05-24 17:23:02 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework15/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework15/Homework.java
@@ -1,6 +1,14 @@
 package ru.gazprombank.educate.homework15;
 
 public class Homework {
+    public static String repeatChar(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            stringBuilder.append(c);
+            stringBuilder.append(c);
+        }
+        return stringBuilder.toString();
+    }
 
     public static void main(String[] args) {
 
