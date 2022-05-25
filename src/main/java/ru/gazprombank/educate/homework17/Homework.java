52946441a84c9884813d71528190f75dc96f73d6
Andrey Fisenko <andrey.fisenko@yahoo.com>
2022-05-25 16:27:44 +0300

    Homework17

--- src/main/java/ru/gazprombank/educate/homework17/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework17/Homework.java
@@ -1,8 +1,20 @@
 package ru.gazprombank.educate.homework17;
 
 public class Homework {
-
     public static void main(String[] args) {
+    String[] words = new String[]{"a", "bb", "b", "ccc"};
+    int len = 3;
+        System.out.println(wordsCount(words, len));
+}
+    public static int wordsCount(String[] words, int len) {
+        int counter = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (len == words[i].length()){
+                counter ++;
+            }
 
+        }
+        return counter;
     }
+
 }
