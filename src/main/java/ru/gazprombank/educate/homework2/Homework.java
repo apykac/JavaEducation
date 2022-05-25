5c7adc9512eab947329205edc3a0f4e002323216
Bochkareva Alina <abochkareva.alina@yandex.ru>
2022-05-25 18:18:48 +0300

    dz

--- src/main/java/ru/gazprombank/educate/homework2/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework2/Homework.java
@@ -1,22 +1,22 @@
 package ru.gazprombank.educate.homework2;
 public class Homework {
-        public static void main(String[] args) throws TriangleExistException {
-            int a = 3;
-            int b = 4;
-            int c = 5;
-            System.out.println("Площадь треугольника:" + calculateTriangleArea(a, b, c));
-        }
+    public static void main(String[] args) throws TriangleExistException {
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        System.out.println("Площадь треугольника:" + calculateTriangleArea(a, b, c));
+    }
 
-        public static double calculateTriangleArea(int a, int b, int c) throws TriangleExistException {
-            double s;
-            if (a + b > c && a + c > b && b + c > a) {
-                double p;
-                p = (a + b + c) / 2;
-                s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-                s = (double) Math.round(s * 1000) / 1000;
-            } else {
-                throw new TriangleExistException();
-            }
-            return s;
+    public static double calculateTriangleArea(int a, int b, int c) throws TriangleExistException {
+        double s;
+        if (a + b > c && a + c > b && b + c > a) {
+            double p;
+            p = (a + b + c) / 2;
+            s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+            s = (double) Math.round(s * 1000) / 1000;
+        } else {
+            throw new TriangleExistException();
         }
+        return s;
+    }
     }
--- src/main/java/ru/gazprombank/educate/homework6/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework6/Homework.java
@@ -1,28 +1,28 @@
 package ru.gazprombank.educate.homework6;
 
 public class Homework {
-        public static int speedingFine (int speed, boolean isHoliday) {
-            int result = 0;
-            if (isHoliday) {
-                if (speed <= 60+5) {
-                    return 0;
-                } else if (speed <= 80+5 && speed > 61+5) {
-                    return 100;
-                } else if (speed > 81+5) {
-                    return 200;
-                }
-            } else {
-                if (speed <= 60) {
-                    return 0;
-                }
-                if (speed <= 80 && speed >= 60) {
-                    return 100;
-                }
-                if (speed > 81) {
-                    return 200;
-                }
+    public static int speedingFine (int speed, boolean isHoliday) {
+        int result = 0;
+        if (isHoliday) {
+            if (speed <= 60+5) {
+                return 0;
+            } else if (speed <= 80+5 && speed > 61+5) {
+                return 100;
+            } else if (speed > 81+5) {
+                return 200;
             }
-            return result;
+        } else {
+            if (speed <= 60) {
+                return 0;
+            }
+            if (speed <= 80 && speed >= 60) {
+                return 100;
+            }
+            if (speed > 81) {
+                return 200;
+            }
         }
+        return result;
     }
+}
 
