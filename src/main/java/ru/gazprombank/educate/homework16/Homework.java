adb1d0ac1b63bd7630bf82e4cc38c315231328e2
Core <zergi89@mail.ru>
2022-05-25 17:54:46 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework16/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework16/Homework.java
@@ -2,7 +2,29 @@ package ru.gazprombank.educate.homework16;
 
 public class Homework {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+            System.out.println(starKill("sdf*ff**"));
+        }
 
+        public static String starKill(String str) {
+            String text = "";
+            boolean star = false;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '*') {
+                    if (!star) {
+                        star = true;
+                        if (text.length() > 1)
+                            text = text.substring(0, text.length() - 1);
+                        else
+                            text = "";
+                    }
+                } else {
+                    if (!star)
+                        text = text + str.charAt(i);
+                    else
+                        star = false;
+                }
+            }
+            return text;
+        }
     }
-}
