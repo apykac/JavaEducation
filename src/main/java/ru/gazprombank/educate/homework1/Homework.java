b8468c545bff8a0fe79b40031d53bf2645888a5f
Bochkareva Alina <abochkareva.alina@yandex.ru>
2022-05-26 14:27:38 +0300

    dz1

--- src/main/java/ru/gazprombank/educate/homework1/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework1/Homework.java
@@ -2,6 +2,21 @@ package ru.gazprombank.educate.homework1;
 
 public class Homework {
     public static void main(String[] args) {
+        int count = Homework.punctuationCount("Hello, what is u name?!");
+        System.out.println(count);
+    }
+    public static int punctuationCount(String a){
+        char[]sign={'!', '?','.', ',' ,':', ';', '/','(', ')', '"','#', '$', '%','&', '\'','*','+', '-','<','=', '>','@','[',']','\\', '`', '_','{','}','~', '|','^'};
+        int count = 0;
+        char[]sign1 = a.toCharArray();
+        for(int i=0;i<sign1.length;i++) {
+            for (int j = 0; j < sign.length; j++) {
+                if (sign1[i] == sign[j]) {
+                    count++;
+                }
+            }
+        }
 
+        return count;
     }
 }
