821d71703d4aa9619145921d3648bcf783e11f58
Core <zergi89@mail.ru>
2022-05-26 23:05:14 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework20/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework20/Homework.java
@@ -1,8 +1,24 @@
 package ru.gazprombank.educate.homework20;
 
 public class Homework {
-
     public static void main(String[] args) {
+        public static void main(String[] args) {
 
+        }
+        public static int yoYo(String str){
+            String i = str;
+            String newStr;
+            if(str.contains("yo")) {
+                newStr = str.replace("yo", "");
+            }
+            else{
+                newStr = str;
+            }
+            if (i.length() != newStr.length()){
+                return  (i.length() - newStr.length()) / 2;
+            }
+            else {
+                return 0;
+            }
+        }
     }
-}
