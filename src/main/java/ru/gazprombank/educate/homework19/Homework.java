ee50a801b239055be5acef506632ec51383120c5
Core <zergi89@mail.ru>
2022-05-26 23:01:30 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework19/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework19/Homework.java
@@ -2,6 +2,22 @@ package ru.gazprombank.educate.homework19;
 
 public class Homework {
 
+    public static int longestStreak(String str) {
+        char[] charArray = str.toCharArray();
+        int temp = 0;
+        int max = 0;
+        for (int i = 0; i < charArray.length; ) {
+            temp = charArray[i];
+            int count = 1;
+            while (++i < charArray.length && charArray[i] == temp)
+                ++count;
+            if (count > max) {
+                max = count;
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
 
     }
