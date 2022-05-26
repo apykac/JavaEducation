f82ed9c7b65b562e167d8d63f33a9ab0b63fd06d
Bochkareva Alina <abochkareva.alina@yandex.ru>
2022-05-26 12:46:59 +0300

    dz6

--- src/main/java/ru/gazprombank/educate/homework9/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework9/Homework.java
@@ -3,6 +3,18 @@ package ru.gazprombank.educate.homework9;
 public class Homework {
 
     public static void main(String[] args) {
+        System.out.println(isOrdered(2,4,5,true));
 
+    }
+
+    public static boolean isOrdered(int first, int second, int third, boolean itsOk) {
+        if (second > first && third > second) {
+            return true;
+        }
+        if (!itsOk && second < third) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
