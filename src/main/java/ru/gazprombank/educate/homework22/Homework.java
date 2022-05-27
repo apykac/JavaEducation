473b425f822d9187b67dac16252fbcb72932a594
Core <zergi89@mail.ru>
2022-05-28 00:37:29 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework22/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework22/Homework.java
@@ -3,6 +3,10 @@ package ru.gazprombank.educate.homework22;
 public class Homework {
 
     public static void main(String[] args) {
-
+        String sc = "hello";
+        String[] = sc.split("-");
+        for (String str) {
+            System.out.println(str);
+        }
     }
 }
