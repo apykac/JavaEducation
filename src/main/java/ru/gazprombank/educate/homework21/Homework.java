af895b6c7767a19d79fc476a9cb43aa3e31bec65
Core <zergi89@mail.ru>
2022-05-28 00:00:41 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework21/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework21/Homework.java
@@ -1,8 +1,22 @@
 package ru.gazprombank.educate.homework21;
 
-public class Homework {
+ public class Homework {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+         String str = "oore";
+         System.out.println(endooo(str));
+     }
 
-    }
-}
+     public static String endooo(String str) {
+         if (str.length() < 1)
+             return "";
+         String newStr = str.substring(1);
+
+         if (str.indexOf("o") == 0) {
+             return endooo(newStr) + "o";
+
+         } else {
+             return str.charAt(0) + endooo(newStr);
+         }
+     }
+ }
