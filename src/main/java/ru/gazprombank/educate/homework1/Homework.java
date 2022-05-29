14d8f3691d272d49e5074f452a0e236b792a5866
Anny <anny270888@gmail.com>
2022-05-29 19:26:36 +0300

    Test

--- src/main/java/ru/gazprombank/educate/homework1/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework1/Homework.java
@@ -1,7 +1,26 @@
 package ru.gazprombank.educate.homework1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Homework {
     public static void main(String[] args) {
+        String text = "fgjhdf./dfjh,!hjg?";
+        System.out.println(punctuationCount(text));
 
+    }
+    public static int punctuationCount(String text){
+        char[] punctuation = ".,!?;:-".toCharArray();
+        char[] s = text.toCharArray();
+        int count = 0;
+        for (int i = 0; i < s.length; i++){
+            for (int j = 0; j < punctuation.length; j++){
+                if (s[i] == punctuation[j]){
+                    count++;
+                }
+            }
+
+        }
+        return count;
     }
 }
