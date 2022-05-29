fd3e8fae89011bf0633d21b507e227481a0d0b07
Bochkareva Alina <abochkareva.alina@yandex.ru>
2022-05-29 17:25:09 +0300

    dz14

--- src/main/java/ru/gazprombank/educate/homework14/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework14/Homework.java
@@ -3,6 +3,21 @@ package ru.gazprombank.educate.homework14;
 public class Homework {
 
     public static void main(String[] args) {
+    }
 
+    public static int partialSum(int a, int b, int c) {
+        int sum = 0;
+        if (a != 13 && b != 13 && c != 13) {
+            sum = a + b + c;
+        }
+        else if (a == 13){
+            sum = 0;
+        }
+        else if (b == 13) {
+            sum = a;
+        }else if (c == 13) {
+            sum = a + b;
+        }
+        return sum;
     }
 }
