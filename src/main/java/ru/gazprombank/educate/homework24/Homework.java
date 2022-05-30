82e9f331292307ec8c8d95ee00a19ef0062ccfa5
Core <zergi89@mail.ru>
2022-05-30 14:41:54 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework24/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework24/Homework.java
@@ -1,8 +1,27 @@
 package ru.gazprombank.educate.homework24;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Homework {
 
     public static void main(String[] args) {
 
+    }
+
+    public static Map <Integer, Integer> getMapFromString (String str) {
+        Map <Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i <= 9; i++) {
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (String.valueOf(str.charAt(j)).equals(String.valueOf(i))) {
+                    count++;
+                }
+            }
+            if (count > 0) {
+                map.put(i, count);
+            }
+        }
+        return map;
     }
 }
