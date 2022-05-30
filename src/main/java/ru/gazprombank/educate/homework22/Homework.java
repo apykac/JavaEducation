bd461e878c8913740ea94e3d68dce37b3476b845
Core <zergi89@mail.ru>
2022-05-30 14:26:12 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework22/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework22/Homework.java
@@ -3,10 +3,19 @@ package ru.gazprombank.educate.homework22;
 public class Homework {
 
     public static void main(String[] args) {
-        String sc = "hello";
-        String[] = sc.split("-");
-        for (String str) {
-            System.out.println(str);
+        String str = "hello";
+        System.out.println(hyphenSplit(String str));
+    }
+
+    public static String hyphenSplit(String str) {
+        String newStr = str.substring(1);
+        if (newStr.length() < 1)
+            return "";
+
+        if (str.charAt(0) == str.charAt(1)) {
+            return str.substring(0, 1) + "-" + hyphenSplit(newStr.substring(0));
+        } else {
+            return str.substring(0, 1) + hyphenSplit(newStr.substring(0));
         }
     }
 }
--- src/main/java/ru/gazprombank/educate/homework23/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework23/Homework.java
@@ -3,6 +3,15 @@ package ru.gazprombank.educate.homework23;
 public class Homework {
 
     public static void main(String[] args) {
+        String str = "xyz[abc]123";
+
+
+
+        }
+
+        public static String insideBrackets(String str) {
+            return str.substring(str.indexOf("["), str.indexOf("]") + 1);
+
+        }
 
     }
-}
