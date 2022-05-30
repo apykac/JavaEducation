76cec99c8bf5c0a82d65fb80b1d3172436ac8990
Bochkareva Alina <abochkareva.alina@yandex.ru>
2022-05-30 11:56:45 +0300

    dz13

--- src/main/java/ru/gazprombank/educate/homework13/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework13/Homework.java
@@ -5,4 +5,18 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+    public static int nearestTwentyOne(int a, int b){
+        int result =0;
+        if ((a<=21)&&(a>b))
+            result = a;
+        else if ((b<=21)&&(b>a))
+            result = b;
+        else if ((a<=21)&&(b>21))
+            result = a;
+        else if ((b<=21)&&(a>21))
+                result = b;
+        else if ((b>21)&&(a>21))
+            result =0;
+        return result;
+    }
 }
