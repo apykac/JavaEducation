833154e6c149d3c2c72906d27039edd5bb3cbf38
Core <zergi89@mail.ru>
2022-05-30 14:30:08 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework23/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework23/Homework.java
@@ -4,14 +4,10 @@ public class Homework {
 
     public static void main(String[] args) {
         String str = "xyz[abc]123";
-
-
-
-        }
-
-        public static String insideBrackets(String str) {
+    }
+    public static String insideBrackets(String str) {
             return str.substring(str.indexOf("["), str.indexOf("]") + 1);
 
         }
 
-    }
+   }
