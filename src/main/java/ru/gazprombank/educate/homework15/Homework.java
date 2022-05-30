dd2ca2ad1630573d7e096f0ac7ee7907926f04c6
Bochkareva Alina <abochkareva.alina@yandex.ru>
2022-05-30 18:46:45 +0300

    dz15

--- src/main/java/ru/gazprombank/educate/homework15/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework15/Homework.java
@@ -5,4 +5,13 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+    public static String repeatChar(String str){
+        char[] string =str.toCharArray();
+        String string1 = "";
+        for (int i = 0; i<string.length; i++){
+            string1 = string1 +string[i]+string[i];
+
+        }
+        return string1;
+    }
 }
