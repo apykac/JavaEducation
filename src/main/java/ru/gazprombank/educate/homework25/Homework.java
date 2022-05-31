753e07c9cfeb6b32cc9fb9174a115db468a87c83
Core <zergi89@mail.ru>
2022-05-31 19:11:47 +0300

    Home v1

--- src/main/java/ru/gazprombank/educate/homework25/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework25/Homework.java
@@ -1,8 +1,26 @@
 package ru.gazprombank.educate.homework25;
 
+import java.util.Arrays
 public class Homework {
 
     public static void main(String[] args) {
+        String[] str = {"hey, hey1, hey man, heyyaaa"};
+        System.out.println(getPrefix(str));
+    }
+
+    public static String getPrefix(String[] strings) {
+       if (strings.length == 0)
+           return "";
+        if (strings.length == 1)
+            return strings[0];
+
+        Arrays.sort(strings);
+        int min = Math.min(strings[0].length(), strings[0].length());
+        int i = 0;
+        while (i < min && strings[0].charAt(i) == strings[1].charAt(i))
+            i++;
+        String prefix = strings[0].substring(0, i);
+        return prefix;
 
     }
 }
