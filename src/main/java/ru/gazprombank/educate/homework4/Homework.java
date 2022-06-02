4b60f74c2e9890dae00f70bf68eef7e3016eb3ae
Discomb <lllkoutalll@gmail.com>
2022-06-02 21:23:58 +0300

    Решена домашка №4

--- src/main/java/ru/gazprombank/educate/homework4/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework4/Homework.java
@@ -1,7 +1,27 @@
 package ru.gazprombank.educate.homework4;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 public class Homework {
     public static void main(String[] args) {
 
+    }
+
+    public static int getRandomInt(int min, int max){
+        Random random = new Random();
+        return random.ints(min, max)
+                .findFirst()
+                .getAsInt();
+    }
+
+    public static int[][] randomMatrix(int[][] inputMatrix) {
+        for (int i = 0; i < inputMatrix.length; i++) {
+            for (int j = 0; j < inputMatrix[i].length; j++) {
+                    inputMatrix[i][j] = getRandomInt(1, 1000000);
+            }
+        }
+
+        return inputMatrix;
     }
 }
