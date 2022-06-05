641bf7f1b8a6c86929941e55f784de88ef9cfacb
Discomb <lllkoutalll@gmail.com>
2022-06-05 10:32:45 +0300

    Добавлено решение домашки №5

--- src/main/java/ru/gazprombank/educate/homework5/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework5/Homework.java
@@ -5,4 +5,17 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static String middleThree(String inText){
+        if (inText.length() <= 3){
+            return inText;
+        } else {
+            int index = inText.length() / 2 -1;
+            int length = 3;
+
+            return inText.substring(index, index+length);
+        }
+
+
+    }
 }
