72a4fdab08a4abc9dff76b6e0f1efd438fedde5b
Discomb <lllkoutalll@gmail.com>
2022-06-05 11:09:54 +0300

    Решена домашка №9

--- src/main/java/ru/gazprombank/educate/homework9/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework9/Homework.java
@@ -5,4 +5,12 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static boolean isOrdered(int first, int second, int third, boolean itsOk){
+        if (itsOk){
+            return second < third;
+        } else {
+            return second > first && third > second;
+        }
+    }
 }
