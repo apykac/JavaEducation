df67148e335914beaa126a70074a7298b922ee88
Discomb <lllkoutalll@gmail.com>
2022-06-05 10:49:20 +0300

    Решена домашка №6

--- src/main/java/ru/gazprombank/educate/homework6/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework6/Homework.java
@@ -5,4 +5,25 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static int speedingFine(int speed, boolean isHoliday){
+        int bonus = 0;
+        int fine;
+
+        if (isHoliday){
+            bonus = 5;
+        }
+
+        int spd = speed - bonus;
+
+        if (spd <= 60){
+            fine = 0;
+        } else if (spd <= 80){
+            fine = 100;
+        } else {
+            fine = 200;
+        }
+
+        return fine;
+    }
 }
