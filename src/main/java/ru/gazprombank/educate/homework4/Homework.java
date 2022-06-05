499b449abfcf1018cb2feb8075880edb95b30f92
Discomb <lllkoutalll@gmail.com>
2022-06-05 10:05:45 +0300

    Исправлено решение домашки №4

--- src/main/java/ru/gazprombank/educate/homework4/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework4/Homework.java
@@ -2,23 +2,35 @@ package ru.gazprombank.educate.homework4;
 
 import java.math.BigInteger;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Homework {
     public static void main(String[] args) {
 
     }
+//
+//    public static int getRandomInt(int min, int max){
+//        Random random = new Random();
+//        return random.ints(min, max)
+//                .findFirst()
+//                .getAsInt();
+//    }
 
     public static int getRandomInt(int min, int max){
-        Random random = new Random();
-        return random.ints(min, max)
-                .findFirst()
-                .getAsInt();
+        Random rand = new Random();
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
     }
 
     public static int[][] randomMatrix(int[][] inputMatrix) {
         for (int i = 0; i < inputMatrix.length; i++) {
             for (int j = 0; j < inputMatrix[i].length; j++) {
-                    inputMatrix[i][j] = getRandomInt(1, 1000000);
+                if (j % 2 == 0) {
+              inputMatrix[i][j] = ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE - 1);
+                } else{
+//              inputMatrix[i][j] = 1 + (int)(Math.random() * ((Integer.MAX_VALUE - 2) + 1));
+                inputMatrix[i][j] = getRandomInt(1, Integer.MAX_VALUE - 1);
+                }
             }
         }
 
