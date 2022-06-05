e62ae852d5464e3028d4fb53182bed803981b7b5
Discomb <lllkoutalll@gmail.com>
2022-06-05 11:01:06 +0300

    Решена домашка №8

--- src/main/java/ru/gazprombank/educate/homework8/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework8/Homework.java
@@ -5,4 +5,15 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static boolean birdsSinging(int temp, boolean isSummer){
+        int upperLimit = 90;
+        int lowerLimit = 60;
+
+        if (isSummer){
+            upperLimit = 100;
+        }
+
+        return lowerLimit <= temp && temp <= upperLimit;
+    }
 }
