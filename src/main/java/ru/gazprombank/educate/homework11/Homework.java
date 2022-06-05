7c3fa5b0d1c52cd8d6c595045eff40b351e81910
Discomb <lllkoutalll@gmail.com>
2022-06-05 11:20:31 +0300

    Решена домашка №11

--- src/main/java/ru/gazprombank/educate/homework11/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework11/Homework.java
@@ -5,4 +5,16 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static String fizzyWizzy(int n){
+        if (n % 3 ==0 && n % 5 ==0){
+            return "FizzBuzz!";
+        } else if (n % 3 == 0){
+            return "Fizz!";
+        } else if (n % 5 == 0){
+            return "Buzz!";
+        } else {
+            return n + "!";
+        }
+    }
 }
