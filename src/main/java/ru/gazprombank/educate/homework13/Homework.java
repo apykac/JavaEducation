3d686ef54ec56cd24ee37fcdb01b25c15a9d0ad5
Discomb <lllkoutalll@gmail.com>
2022-06-06 23:50:04 +0300

    Решена домашка №13

--- src/main/java/ru/gazprombank/educate/homework13/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework13/Homework.java
@@ -5,4 +5,25 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static int nearestTwentyOne(int a, int b){
+
+        if (a > 21 && b > 21) {
+            return 0;
+        } else if (a > 21) {
+            return b;
+        } else if (b > 21){
+            return a;
+        }
+
+        int i = 21 - a;
+        int j = 21 - b;
+
+        if (i < j) {
+            return a;
+        } else {
+            return b;
+        }
+
+    }
 }
