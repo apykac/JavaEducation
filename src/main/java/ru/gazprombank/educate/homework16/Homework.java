6e59ea80309a48797b8e369e67e00a178fc55786
Discomb <lllkoutalll@gmail.com>
2022-06-07 19:56:49 +0300

    Решена домашка №16

--- src/main/java/ru/gazprombank/educate/homework16/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework16/Homework.java
@@ -5,4 +5,8 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static String starKill(String str){
+        return str.replaceAll("(.{0,1}\\*+.{0,1})", "");
+    }
 }
