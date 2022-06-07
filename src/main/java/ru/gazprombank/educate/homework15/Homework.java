88dc6b0cfa8635ae087e3e745185e3951478e6f0
Discomb <lllkoutalll@gmail.com>
2022-06-07 19:30:28 +0300

    Решена домашка №15

--- src/main/java/ru/gazprombank/educate/homework15/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework15/Homework.java
@@ -5,4 +5,19 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static String repeatChar(String str){
+
+        char[] inStr = str.toCharArray();
+
+        String finStr = "";
+
+        for (char c : inStr) {
+            for (int j = 0; j < 2; j++) {
+                finStr += c;
+            }
+        }
+
+        return finStr;
+    }
 }
