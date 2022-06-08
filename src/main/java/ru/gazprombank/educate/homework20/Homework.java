c2b4616873f3d62a2c82c79638373dfef71dbd8f
Discomb <lllkoutalll@gmail.com>
2022-06-08 21:44:00 +0300

    Решена домашка №20

--- src/main/java/ru/gazprombank/educate/homework20/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework20/Homework.java
@@ -5,4 +5,16 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static int yoYo(String str){
+        String yo = "yo";
+
+        if (!str.contains(yo)) {
+            return 0;
+        } else if (str.substring(0, yo.length()).equals(yo)){
+            return yoYo(str.substring(1)) + 1;
+        } else {
+            return yoYo(str.substring(1));
+        }
+    }
 }
