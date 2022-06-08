238e944fd3bbbee4077e16fdab1951de45305766
Discomb <lllkoutalll@gmail.com>
2022-06-08 11:01:23 +0300

    Решена домашка №18

--- src/main/java/ru/gazprombank/educate/homework18/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework18/Homework.java
@@ -5,4 +5,22 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static int matchingChar(String[] a, String[] b){
+        int count = 0;
+        int i = 0;
+
+        for(String s : a){
+            if (s.length() != 0){
+                if (b[i].length() != 0){
+                    if (s.substring(0, 1).equals(b[i].substring(0,1))){
+                        count++;
+                    }
+                }
+            }
+            i++;
+        }
+        
+        return count;
+    }
 }
