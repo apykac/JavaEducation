a91334a37ebfd3178d34bede45321716da6b6f15
Discomb <lllkoutalll@gmail.com>
2022-06-08 11:32:30 +0300

    Решена домашка №19

--- src/main/java/ru/gazprombank/educate/homework19/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework19/Homework.java
@@ -5,4 +5,31 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static int longestStreak(String str){
+        int len = 0;
+        char prev;
+        int longest = 0;
+
+        if (str.length() != 0) {
+            prev = str.toCharArray()[0];
+            len = 0;
+            for (char c : str.toCharArray()) {
+                if (c == prev){
+                    len++;
+                } else {
+                    if (len >= longest){
+                        longest = len;
+                    }
+                    prev = c;
+                    len = 1;
+                }
+            }
+            if (len >= longest){
+                longest = len;
+            }
+        }
+
+        return longest;
+    }
 }
