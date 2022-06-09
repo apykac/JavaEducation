8cbb4c118943deea027777ecc46002a9d649f45c
Discomb <lllkoutalll@gmail.com>
2022-06-09 21:29:10 +0300

    Решена домашка №21

--- src/main/java/ru/gazprombank/educate/homework21/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework21/Homework.java
@@ -5,4 +5,18 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static String endoo(String str){
+        char o = 'o';
+
+        if (str.length() < 1) {
+            return "";
+        }
+
+        if (str.indexOf(o) == 0 && !str.matches("[o]*")){
+            return endoo(str.substring(1) + o);
+        } else {
+            return str.charAt(0) + endoo(str.substring(1));
+        }
+    }
 }
