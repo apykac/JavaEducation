12fea5ff5f1db3ab43799fad04329aa77a4a4ee3
Discomb <lllkoutalll@gmail.com>
2022-06-11 17:27:44 +0300

    Решена домашка №23

--- src/main/java/ru/gazprombank/educate/homework23/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework23/Homework.java
@@ -1,8 +1,23 @@
 package ru.gazprombank.educate.homework23;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Homework {
 
     public static void main(String[] args) {
+
+    }
+
+    public static String insideBrackets(String str){
+        Pattern pattern = Pattern.compile("\\[.*]");
+        Matcher matcher = pattern.matcher(str);
+
+        if (matcher.find()){
+            return matcher.group();
+        } else {
+            return str;
+        }
 
     }
 }
