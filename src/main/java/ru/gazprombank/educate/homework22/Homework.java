c487468f2fd1c57c58fe38b1e8c45fe9616a3ba5
Discomb <lllkoutalll@gmail.com>
2022-06-11 16:37:07 +0300

    Решена домашка №22

--- src/main/java/ru/gazprombank/educate/homework22/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework22/Homework.java
@@ -5,4 +5,20 @@ public class Homework {
     public static void main(String[] args) {
 
     }
+
+    public static String hyphenSplit(String str){
+        String hyp = "-";
+
+        if (str.length() < 2) {
+            return str;
+        }
+
+        if (str.charAt(0) == str.charAt(1)){
+            return hyphenSplit(str.charAt(0) + hyp + str.substring(1));
+        } else {
+            return str.charAt(0) + hyphenSplit(str.substring(1));
+        }
+
+
+    }
 }
