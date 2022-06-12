54c8b4a87c38d56d1a7a6682cd4cf61686bdeb9f
Discomb <lllkoutalll@gmail.com>
2022-06-12 11:22:56 +0300

    Решена домашка №24

--- src/main/java/ru/gazprombank/educate/homework24/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework24/Homework.java
@@ -1,8 +1,30 @@
 package ru.gazprombank.educate.homework24;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Homework {
 
     public static void main(String[] args) {
 
     }
+
+    public static Map<Integer, Integer> getMapFromString(String str){
+        Map<Integer, Integer> innerMap = new HashMap<>();
+
+        for (char c : str.toCharArray()){
+
+            if (!innerMap.containsKey(Character.getNumericValue(c))){
+                if (Character.isDigit(c)) {
+                    innerMap.put(Character.getNumericValue(c), 1);
+                }
+            } else {
+                innerMap.merge(Character.getNumericValue(c), 1, Integer::sum);
+            }
+        }
+
+        return innerMap;
+
+    }
+
 }
