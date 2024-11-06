158566e19a946f122dbe9fcd1da02c468f810e5c
vesni <vesninroman@yandex.ru>
2024-11-06 18:28:16 +0300

    3 задание еще вариантик

--- src/main/java/ru/gazprombank/educate/homework3/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework3/Homework.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Homework {
     public static void main(String[] args) {
-       // int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-       // printMatrix(a);
+        int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        printMatrix(a);
     }
-    public static void printMatrix(int[][] array) {
+/*    public static void printMatrix(int[][] array) {
     for(int i = 0; i < array.length; i++){
         for(int j = 0; j < array[i].length; j++){
             System.out.print(array[i][j]);
@@ -21,10 +21,37 @@ public class Homework {
             break;
         }
         else {
-            System.out.println();
+            System.out.println();*/
 
-}
+    /*}
 
+        }
+        }*/
+    static String stringFinal = "";
+    public static void printMatrix(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            String str  = Arrays.toString(array[i]);
+            str = str.replaceAll("[\\[\\],]", "");
+            if (i == array.length - 1) {
+                stringFinal += str;
+            } else {
+                stringFinal = stringFinal + str + "\n";
+            }
+        }
+             System.out.print(stringFinal);
+
+/*            for (int j = 0; j < array[i].length; j++) {
+                if (j == (array[i].length - 1)) {
+                    break;
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            if (i == (array.length - 1)) {
+                break;
+            } else {
+                System.out.println();
+
+            }*/
+        }
     }
-    }
-}
