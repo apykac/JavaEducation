defe6f8a3950f66c045f7dc3d754e9647fefc512
vesni <vesninroman@yandex.ru>
2024-11-11 18:14:43 +0300

    3 задание еще вариантик

--- src/main/java/ru/gazprombank/educate/homework15/Homework.java
+++ src/main/java/ru/gazprombank/educate/homework15/Homework.java
@@ -8,15 +8,13 @@ public class Homework {
     }
     public static String repeatChar(String str){
 
-        char[] array = str.toCharArray();
-       // char[] arrayResult = new char[str.length() * 2];
+
         StringBuffer buffer = new StringBuffer();
 
-        for(int c = 0; c < array.length; c++){
-            StringBuffer append;
-            append = buffer.append(array[c] * 2);
-
-            //arrayResult = (char) (array[c] + array[c]);
+        for(int c = 0; c < str.length(); c++){
+            char i = str.charAt(c);
+            buffer.append(i);
+            buffer.append(i);
         }
 
         return String.valueOf(buffer);
